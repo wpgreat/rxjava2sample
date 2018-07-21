a8e669c7fafa9499fe1c6aa0ec3356e25fc1da93
chaomi <chaomi@2dfire.com>
2018-07-21 13:33:49 +0800

    add testBase method

--- src/test/java/com/service/VideoRatingServiceTest.java
+++ src/test/java/com/service/VideoRatingServiceTest.java
@@ -2,6 +2,11 @@ package com.service;
 
 
 import com.model.VideoRating;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,8 +18,49 @@ public class VideoRatingServiceTest {
             = new VideoRatingService(new RatingService(), new VideoService());
 
     @Test
-    public void shouldRetrieveVideoRatings() {
+    public void testGetAllVideoRatings() {
         List<VideoRating> videoRatings = videoRatingService.getAllVideoRatings();
         Assert.assertEquals(4, videoRatings.size());
+    }
+
+    @Test
+    public void testBase() {
+        // 创建一个上游 Observable
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                //emitter.onComplete();
+                emitter.onError(new Throwable("Error occurred!"));
+                emitter.onNext(4);
+                emitter.onNext(5);
+            }
+        });
+        // 创建一个下游 Observer
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext:" + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError:" + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+        // 建立连接
+        observable.subscribe(observer);
     }
 }
